1121b3d8c2bbbf1a4400693732a0268e50deaeaf
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-02-20 13:55:10 -0800

    blackjack_score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,26 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
+FACECARDS = ["King", "Queen", "Jack"]
 
 def blackjack_score(hand)
+  hand.each do |card|
+    unless VALID_CARDS.include?(card)
+      raise ArgumentError, "Invalid card."
+    end
+    if FACECARDS.include?(card)
+      card = 10
+    end
+    score += card
+  end
 
+  if hand < 2 || hand > 5
+    raise ArgumentError, "Hand size must be between and including 2-5."
+  end
+
+  return score
 end
+
+# puts blackjack_score()
+
+# hand = an array
